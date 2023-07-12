8836e23f37bfdea1f359000a0d0570728f78c0d4
Madu <93155145+maria-silvas@users.noreply.github.com>
2023-07-12 09:42:53 -0300

    atualização login.js

--- demo-10-Autenticação e Autorização/src/login.js
+++ demo-10-Autenticação e Autorização/src/login.js
@@ -1,56 +1,63 @@
-const users = require('./../db/users.json')
-const JWT_KEY = process.env.JWT_KEY
-const { sign } = require('jsonwebtoken')
+const users = require('./../db/users.json');
+const JWT_KEY = process.env.JWT_KEY;
+const { sign } = require('jsonwebtoken');
 
-const login = async event => {
-    console.log('Login invoked!..', new Date().toISOString(), event.body)
+const generateToken = (user) => {
+  const signUser = {
+    scopes: user.scopes,
+    username: user.username
+  };
 
-    // Extrai o nome de usuário e senha do corpo da solicitação
-    const {
-        username,
-        password
-    } = JSON.parse(event.body)
+  return sign({ user: signUser }, JWT_KEY, { expiresIn: '5m' });
+};
 
-    // Realiza as validações necessárias (não especificadas no código fornecido)
+const login = async (event) => {
+  console.log('Login invoked!..', new Date().toISOString(), event.body);
 
-    // Procura pelo usuário válido nos dados armazenados (users)
-    const validUser = users.find(
-        user =>
-            user.username.toLowerCase() === username.toLowerCase() &&
-            user.password === password
-    )
+  // Extrai o nome de usuário e senha do corpo da solicitação
+  const { username, password } = JSON.parse(event.body);
 
-    // Se o usuário não for válido, retorna uma resposta com status 401 (não autorizado)
-    if (!validUser) {
-        return {
-            statusCode: 401,
-            body: JSON.stringify({
-                message: 'Unauthorized'
-            })
-        }
-    }
-
-    // Prepara os dados do usuário para assinar o token JWT
-    const signUser = {
-        scopes: validUser.scopes,
-        username: validUser.username
-    }
-
-    // Gera um token JWT com uma chave JWT (JWT_KEY) e um tempo de expiração de 5 minutos
-    const token = sign({
-        user: signUser
-    }, JWT_KEY, { expiresIn: '5m' })
-
-    // Retorna uma resposta com status 200 e o token JWT no corpo da resposta
+  // Verifica se o nome de usuário e a senha foram fornecidos
+  if (!username || !password) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({
-            token
-        })
-    }
-}
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Username and password are required'
+      })
+    };
+  }
+
+  // Procura pelo usuário válido nos dados armazenados (users)
+  const authenticatedUser = users.find(
+    (user) =>
+      user.username.toLowerCase() === username.toLowerCase() &&
+      user.password === password
+  );
+
+  // Se o usuário não for válido, retorna uma resposta com status 401 (não autorizado)
+  if (!authenticatedUser) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: 'Unauthorized'
+      })
+    };
+  }
+
+  // Gera um token JWT com base nos dados do usuário autenticado
+  const token = generateToken(authenticatedUser);
+
+  // Retorna uma resposta com status 200 e o token JWT no corpo da resposta
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      token
+    })
+  };
+};
 
 // Exporta a função "login" como o manipulador da função Lambda
 exports.handler = login;
 
-
+//Nesta versão aprimorada, adicionei a validação para garantir que o nome de usuário e a senha sejam fornecidos na solicitação. 
+//Caso contrário, uma resposta de erro com status HTTP 400 (Bad Request) será retornada.

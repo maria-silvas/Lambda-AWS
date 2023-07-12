883499a3243dd3bd8166fff42b42e2f7481fc918
Madu <93155145+maria-silvas@users.noreply.github.com>
2023-07-12 09:30:44 -0300

    Melhorias getHeroes.js

--- demo-10-Autenticação e Autorização/src/getHeroes.js
+++ demo-10-Autenticação e Autorização/src/getHeroes.js
@@ -1,62 +1,48 @@
 'use strict';
 
 // Rota pública
-module.exports.public = async event => {
-    console.log(
-        'Requesting public route...', new Date().toISOString()
-    )
+module.exports.public = async (event) => {
+console.log('Requesting public route...', new Date().toISOString());
 
-    // Retorna uma resposta com status 200 e um array de objeto JSON como corpo da resposta
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            [
-                {
-                    id: 1,
-                    name: "Flash",
-                    power: "Speed"
-                }
-            ],
-            null,
-            2
-        ),
-    };
+const response = {
+statusCode: 200,
+body: JSON.stringify([
+{
+id: 1,
+name: "Flash",
+power: "Speed"
+}
+], null, 2)
+};
+
+return response;
 };
 
 // Rota privada
-module.exports.private = async event => {
+module.exports.private = async (event) => {
+console.log('Requesting private route...', new Date().toISOString());
 
-    console.log(
-        'Requesting private route...', new Date().toISOString()
-    )
-    console.log(
-        {
-            'User': JSON.parse(
-                event.requestContext.authorizer.user
-            )
-        }
-    )
+const user = JSON.parse(event.requestContext.authorizer.user);
+console.log({ 'User': user });
 
-    // Retorna uma resposta com status 200 e um array de objeto JSON como corpo da resposta
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            [
-                {
-                    id: 100,
-                    name: "Batman",
-                    power: "Rich"
-                }
-            ],
-            null,
-            2
-        ),
-    };
+const response = {
+statusCode: 200,
+body: JSON.stringify([
+{
+id: 100,
+name: "Batman",
+power: "Rich"
+}
+], null, 2)
 };
-//Neste código, temos duas funções exportadas: public e private, que representam rotas públicas e privadas, respectivamente.
 
-//A função public é uma rota pública que imprime uma mensagem de log indicando a solicitação da rota e retorna uma resposta com status 200 e um array JSON no corpo da resposta.
+return response;
+};
 
-//A função private é uma rota privada que imprime uma mensagem de log indicando a solicitação da rota, extrai os detalhes do usuário do objeto event (que contém o contexto da requisição) e retorna uma resposta com status 200 e um array JSON no corpo da resposta.
+//Principais melhorias:
 
-//Espero que isso esclareça o código para você. Se você tiver mais perguntas, fique à vontade para perguntar.
+//Adicionei comentários para explicar a função de cada rota.
+
+//Atribuí a resposta em um objeto response antes de retorná-la. Isso torna o código mais legível e facilita possíveis modificações futuras.
+
+//Acessamos diretamente a propriedade authorizer.user do objeto event.requestContext, sem a necessidade de chamar JSON.parse várias vezes.
